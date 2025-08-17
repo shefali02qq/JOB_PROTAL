b82d931c8d0342eee81dc0bb0d7255feac450cd7
shefali02qq <shefalisingh870@gmail.com>
2025-08-18 01:17:45 +0530

    Updated url for deployemnet

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-protal-fbgo.onrender.com',
     credentials:true
 }
 
