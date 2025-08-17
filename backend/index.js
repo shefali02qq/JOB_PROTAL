651e7517eac6b83e69a4c6f0195974002cf1cef5
shefali02qq <shefalisingh870@gmail.com>
2025-08-18 00:30:44 +0530

    Updated backend and frontend packages, modified index.js

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,12 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
 const app = express();
+const __dirname=path.resolve();
 
 // middleware
 app.use(express.json({ limit: '10mb' }));
@@ -32,6 +34,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 app.listen(PORT,()=>{
